da1988219caf9590cb66353cc9f3c8a663513150
Lalitus <tallitosan@gmail.com>
2020-06-09 04:32:12 -0500

    add public folder with css folder and html files

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,27 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(
+        path.join(rootDir, 'views','add-product.html')
+    );
 });// to add middlewares, next param is a function
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);// body is added by express
+    // console.log(req.body);// body is added by express
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,22 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
+// router.get('/', (req, res, next) => {
+//     // console.log('In another middleware!');
+//     res.send('<h1>Hello from Express</h1>');
+// });// to add middlewares, next param is a function
+
 router.get('/', (req, res, next) => {
-    // console.log('In another middleware!');
-    res.send('<h1>Hello from Express</h1>');
-});// to add middlewares, next param is a function
+    console.log('shop.js', adminData.products);
+    res.sendFile(
+        path.join(rootDir, 'views', 'shop.html')
+    );
+});
 
 module.exports = router;
